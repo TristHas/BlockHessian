25a2e5b7bbca48490dfad00f9899788743406959
YoFuse <yfuse42@gmail.com>
2019-10-20 16:45:16 +0900

    update the lr searching way

--- Pytorch/lr_calibration/lr_calibrate.py
+++ Pytorch/lr_calibration/lr_calibrate.py
@@ -1,5 +1,8 @@
+import sys
 import numpy as np
 import torch
+sys.path.append("../src")
+
 from block_analysis import block_hessian, curvature_effects
 
 def pp(lr, delta, h, H):
@@ -15,25 +18,28 @@ def lr_range(model, ds, loss_fn, start=-8, stop=8, step=1, log_scale=False):
         delta, h = curvature_effects(model, ds, loss_fn, lr)
         pp(lr, delta, h, H)
         
-def lr_search(model, ds, loss_fn, start=-8, stop=8, step=1, log_scale=False):
+def lr_search(model, ds, loss_fn, start=-8, stop=8, step=1, log_scale=False, print_log=False, th=1.e-4):
     lr_list = []
     error_list = []
     H_list = []
-    
+     
     for lr in np.arange(start, stop, step):
         if log_scale:
             lr = 10.**lr
         H = block_hessian(model, ds, loss_fn, lr)
         delta, h = curvature_effects(model, ds, loss_fn, lr)
-        pp(lr, delta, h, H)
+        if print_log:
+            pp(lr, delta, h, H)
         rel = abs((h.item() - H.sum().item()) / min(abs(H.sum().item()), abs(h.item())))
         
+        if log_scale:
+            lr = np.log10(lr)
         lr_list.append(lr)
         error_list.append(rel)
         H_list.append(H.sum().item())
-        
+    
     diff = np.diff(H_list)
-    diff = abs(diff)<1e-4
+    diff = abs(diff)<th
     diff = np.insert(diff, 0, False)
     
     error_list = np.array(error_list)
@@ -53,14 +59,22 @@ def lr_search(model, ds, loss_fn, start=-8, stop=8, step=1, log_scale=False):
         
     return inf, sup, best
     
-def lr_calibrate(model, ds, loss_fn, start=-8, stop=8, step=1, log_scale=False, width=1.e-4):
+def lr_calibrate(model, ds, loss_fn, start=-8, stop=8, step=1, log_scale=False, print_log=False, width=1.e-3):
     
-    inf, sup, best = lr_search(model, ds, loss_fn, start, stop, step, log_scale)
-    print(inf,best,sup)
+    inf, sup, best = lr_search(model, ds, loss_fn, start, stop, step, log_scale, print_log)
+    if print_log:
+        if log_scale:
+            print(10.**inf, 10.**best, 10.**sup)
+        else:
+            print(inf,best,sup)
     
     while (sup-inf)>width:
         step = (sup-inf)/10
-        inf, sup, best = lr_search(model, ds, loss_fn, inf, sup+step, step, False)
-        print(inf,best,sup)
+        inf, sup, best = lr_search(model, ds, loss_fn, inf, sup+step, step, log_scale, print_log)
+        if print_log:
+            if log_scale:
+                print(10.**inf, 10.**best, 10.**sup)
+            else:
+                print(inf,best,sup)
         
-    return best
+    return best if not log_scale else 10.**best
